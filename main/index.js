dbd39c1cfc69ab019e1a7c3ee1e4e02691f9e4e2
Sushanta65 <sushanta9119@gmail.com>
2025-04-02 23:43:15 +0600

    user api creation done

--- main/index.js
+++ main/index.js
@@ -5,19 +5,26 @@ const cors = require("cors");
 // Load environment variables
 dotenv.config();
 
-
+const db = 'database'
 
 // Initialize Express app
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON request bodies
+app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Your App is Running Properly.')
 })
+
+app.post('/api/users', async(req, res) => {
+  const user = req.body;
+  const result = await db.insertOne(user)
+  res.send(result)
+})
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
